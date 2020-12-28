6a96cf229a671b48f4ceeeba95eed84cec7deae4
mariaalonso2705uni <alonsogm@tcd.ie>
2020-12-28 19:05:50 +0100

    comparison implemented

--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -10,8 +10,11 @@ import { OrganizationService } from './../services/organizations.services';
 export class OrganizationComponent implements OnInit {
 
   login: FormControl;
+  login2: FormControl;
   details = [];
+  details2 = [];
   status = false;
+  status2 = false;
 
   constructor(private organizationService: OrganizationService) { }
 
@@ -24,6 +27,16 @@ export class OrganizationComponent implements OnInit {
         this.details = result;
         console.log(this.details);
         this.status = true;
+      }
+    );
+  }
+
+  getOrganizationDetails2() {
+    this.organizationService.getOrganization(this.login2).subscribe(
+      (result) => {
+        this.details2 = result;
+        console.log(this.details);
+        this.status2 = true;
       }
     );
   }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,9 +10,12 @@ import { FormControl } from '@angular/forms';
 export class UserComponent implements OnInit {
 
   username: FormControl;
+  username2: FormControl;
   countRepositories: any;
   repositories = [];
+  repositories2 = [];
   status = false;
+  status2 = false;
 
   constructor(private userService: UserService) { }
 
@@ -24,6 +27,14 @@ export class UserComponent implements OnInit {
         this.countRepositories = result.length;
         this.repositories = result;
         this.status = true;
+      });
+  }
+  searchUser2() {
+    this.userService.getUser(this.username2).subscribe (
+      (result2) => {
+        this.countRepositories = result2.length;
+        this.repositories2 = result2;
+        this.status2 = true;
       });
   }
 }
